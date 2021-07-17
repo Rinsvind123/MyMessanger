4a2f3e4ae6b4f97885479544ecf1cb8f24fa07be
Коренев Артур <tura9@mail.ru>
2021-07-18 03:42:10 +0800

    клиент в консоли готов

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,20 +1,43 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace MyMessanger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
+
         static void Main()
         {
-            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deserializedMsg);
-
-            //{ "UserName":"RusAl","MessageText":"Privet","TimeStamp":"2021-07-17T18:29:35.4286738Z"}
-            //RusAl < 17.07.2021 18:29:35 >: Privet
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            //UserName = "RusAl";
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
     }
 }
