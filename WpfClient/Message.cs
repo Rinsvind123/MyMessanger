03a81c56f2ea244c8737ea54f82744cf65ba445f
Коренев Артур <tura9@mail.ru>
2021-07-18 17:23:06 +0800

    WPF клиент готов

---
+++ WpfClient/Message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyMessanger
+{
+    [Serializable]
+
+    public class Message
+    {
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public string MessageText
+        {
+            get;
+            set;
+        }
+        public DateTime TimeStamp
+        {
+            get;
+            set;
+        }
+
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = string.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
